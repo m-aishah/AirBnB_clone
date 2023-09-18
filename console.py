6b928c041a49c32b5491240ea3d60430f72c2a6f
Aishah <maishah2540@gmail.com>
2023-09-17 18:41:18 -0700

    Trying to get more checks

--- console.py
+++ console.py
@@ -6,6 +6,7 @@ import re
 import models
 import json
 from models.base_model import BaseModel
+from models.user import User
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
@@ -29,6 +30,7 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
     __classes = [
             'BaseModel',
+            'User',
             'State',
             'City',
             'Amenity',
@@ -39,6 +41,27 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         '''Do nothing upon receiving an empty line.'''
         pass
+
+    def default(self, line):
+        '''This method runs when an invalid command is inputted.'''
+        commands = {
+            'all': self.do_all,
+            'show': self.do_show,
+           'destroy':self.do_destroy,
+            'update': self.do_update
+        }
+        pattern = r'\.'
+        match = re.search(pattern, line)
+        if match is not None:
+            args_list = [line[:match.span()[0]], line[match.span()[1]:]]
+            pattern = r'\((.*?)\)'
+            match = re.search(pattern, args_list[1])
+            if match is not None:
+                command = [args_list[1][:match.span()[0]], match.group()[1:-1]]
+                if command[0] in commands.keys():
+                    s = '{} {}'.format(args_list[0], command[1])
+                    return commands[command[0]](s)
+        return cmd.Cmd.default(self, line)
 
     def do_quit(self, line):
         '''Quit command to exit the program.'''
@@ -141,6 +164,16 @@ class HBNBCommand(cmd.Cmd):
                 obj.__dict__[args[2]] = args[3]
         models.storage.save()
 
+    def do_count(self, line):
+        '''Usage: count <class_name> or <class_name>.count()
+        Print the number of instances of a given class.
+        '''
+        args = parse(line)
+        count = 0
+        for obj in models.storage.all().values():
+            if args[0] == obj.__class__.__name__:
+                count += 1
+        print(count)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
